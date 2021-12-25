b847f3fdbcfd83b7de1b8ec45fb88690e0f3cda5
Wellington José Leite da Silva <wellington.71319@gmail.com>
2021-12-25 14:49:03 -0300

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -102,14 +102,14 @@ xhost +local:root > /dev/null 2>&1
 # Enable checkwinsize so that bash will check the terminal size when
 # it regains control.  #65623
 # http://cnswww.cns.cwru.edu/~chet/bash/FAQ (E11)
-shopt -s checkwinsize
+#shopt -s checkwinsize
 
-shopt -s expand_aliases
+#shopt -s expand_aliases
 
 # export QT_SELECT=4
 
 # Enable history appending instead of overwriting.  #139609
-shopt -s histappend
+#shopt -s histappend
 
 #
 # # ex - archive extractor
